c35e1787ae67aa0a473af268b467e6eeb044777c
Warlly Machado <105324001+Swiziman@users.noreply.github.com>
2024-06-05 19:39:43 -0400

    Atualização

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import registro_movimentacao
-
+from django.db import models
 
 admin.site.register(registro_movimentacao)
+#class registro_movimentacao(models.Model):
+ #   campo_exemplo = models.CharField(max_length=255, help_text="Dica: Este é um campo de exemplo. Insira as informações conforme necessário.")
+    # outros campos
--- core/models.py
+++ core/models.py
@@ -6,3 +6,10 @@ class registro_movimentacao(models.Model):
     raspberry_id = models.IntegerField()
     dispositivo_id = models.CharField(max_length=36)
     closed = models.BooleanField(null=True, default=True)
+
+#class registro_movimentacao(models.Model):
+    #campo_exemplo = models.CharField(max_length=255, help_text="Dica: Este é um campo de exemplo. Insira as informações conforme necessário.")
+    # outros campos
+
+   # def __str__(self):
+   #     return self.campo_exemplo
